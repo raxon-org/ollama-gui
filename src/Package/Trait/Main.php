c253ad59e3f7df804c7cc917b7d10366d13665bd
Remco van der Velde <remco@universeorange.com>
2026-04-17 16:48:11 +0200

    init

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -4,9 +4,13 @@ namespace Package\Raxon\Ollama\Gui\Trait;
 use Raxon\App;
 use Raxon\Config;
 
+use Raxon\Doctrine\Module\Database;
+use Raxon\Doctrine\Module\Entity;
 use Raxon\Exception\DirectoryCreateException;
 
+use Raxon\Exception\ObjectException;
 use Raxon\Module\Cli;
+use Raxon\Module\Controller;
 use Raxon\Module\Data;
 use Raxon\Module\Dir;
 use Raxon\Module\Core;
@@ -197,6 +201,104 @@ trait Main {
         if($notification){
             echo $notification;
         }
+        $list = $this->user_list('ROLE_ADMIN');
+        $this->navigation_create($list);
+    }
+
+    /**
+     * @throws ObjectException
+     * @throws Exception
+     */
+    public function navigation_create($list): void
+    {
+        $object = $this->object();
+        if(array_key_exists('nodeList', $list)){
+            foreach($list['nodeList'] as $nr => $user){
+                if(array_key_exists('uuid', $user)){
+                    $node = new Node($object);
+                    $class = 'Application.Desktop.Navigation';
+                    $role = $node->role_system();
+                    $response = $node->record(
+                        $class,
+                        $role,
+                        [
+                            'where' => [
+                                [
+                                    'attribute' => 'name',
+                                    'operator' => '===',
+                                    'value' => self::NAME,
+                                ],
+                                'and',
+                                [
+                                    'attribute' => 'user',
+                                    'operator' => '===',
+                                    'value' => $user['uuid'],
+                                ]
+                            ],
+                            'relation' => false
+                        ]
+                    );
+                    if($response === null){
+                        $record = [
+                            "name" => self::NAME,
+                            "user" => $user['uuid'],
+                            "route" => (object) [
+                                'name' => 'application.file.manager',
+                                'get' => '{{route.name($this.name)}}'
+                            ],
+                            "url" => '{{route.get($this.route.get)}}',
+                            "svg" => '/Application/' . self::NAME . '/Icon/Icon.png',
+                            "icon" => '/Application/' . self::NAME . '/Icon/Icon.png'
+                        ];
+                        $response = $node->create($class, $role, $record);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @throws ObjectException
+     * @throws Exception
+     */
+    public function user_list($role=''): array
+    {
+        $object = $this->object();
+        $list = [];
+        switch($role){
+            case 'ROLE_ADMIN':
+                $node = new Node($object);
+                $class = 'Account.Role';
+                $role = $node->role_system();
+                $response = $node->record($class, $role, [
+                    'filter' => [
+                        'name' => 'ROLE_ADMIN'
+                    ]
+                ]);
+                if(
+                    array_key_exists('node', $response) &&
+                    is_object($response['node'])
+                ){
+                    $table = 'user';
+                    $options = (object) [];
+                    $options->relation = true;
+                    $config = Database::config($object);
+                    $connection = $object->config('doctrine.environment.system.*');
+                    $em = Database::entity_manager($object, $config, $connection);
+                    $entity = str_replace('.', '', Controller::name($table));
+                    $object->request('entity', $entity);
+                    $object->request('where', [
+                        [
+                            'attribute' => 'role',
+                            'operator' => '===',
+                            'value' => 'JSON_CONTAINS("' . $entity  .'.role", "' .$response['node']->uuid.'") = 1'
+                        ]
+                    ]);
+                    $list = Entity::list($object, $em, $node->role_system(), $options);
+                }
+                break;
+        }
+        return $list;
     }
 
 }
