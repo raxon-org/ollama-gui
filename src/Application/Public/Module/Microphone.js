dbe8a3fdd7ce6e24f0c190cb32160e053ac980c2
Remco van der Velde <remco@universeorange.com>
2026-05-07 11:44:39 +0200

    ` init`

--- src/Application/Public/Module/Microphone.js
+++ src/Application/Public/Module/Microphone.js
@@ -148,7 +148,7 @@ microphone.init = (section_id) => {
                         }, (url, response) => {
                             if(response?.url && response?.name){
                                 //let menu = select('section[name="application-desktop-navigation"] nav');
-                                let item = select('section[name="application-desktop-navigation"] a[data-url="' + response.url + '"]');
+                                let item = select('section[name="application-desktop-navigation"] .menu a[data-url="' + response.url + '"]');
                                 console.log(item);
                                 if(item){
                                     item.trigger('click');
