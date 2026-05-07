e59d7aa7f35644e9150aea417e6e5662197a3b66
Remco van der Velde <remco@universeorange.com>
2026-05-07 11:30:19 +0200

    ` init`

--- src/Application/Public/Module/Microphone.js
+++ src/Application/Public/Module/Microphone.js
@@ -147,7 +147,6 @@ microphone.init = (section_id) => {
                             "url": file.data.get('route.backend.target') + filename,
                         }, (url, response) => {
                             if(response?.url && response?.name){
-                                /*
                                 let a = _('_').create('a');
                                 a.data('url', response.url);
                                 a.data('method', '');
@@ -157,7 +156,6 @@ microphone.init = (section_id) => {
                                 //<a data-url="https://admin.workandtravel.local/Application/Filemanager/" data-method="" data-target="" data-header-with-authorization="true" data-mtime="1778145542.239"><li class="item"><p><img src="/Application/Filemanager/Icon/Icon.png" alt="icon" class="icon">Filemanager</p></li></a>
                                 // request(response.url);
                                 console.log('request made');
-                                 */
                             } else {
                                 let prompt = select('#' + section_id + ' [name="prompt"]');
                                 prompt.val(response);
