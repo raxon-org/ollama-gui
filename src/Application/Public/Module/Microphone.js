a9c9ea518b6414034062d0826bbc797216b7edc4
Remco van der Velde <remco@universeorange.com>
2026-05-07 11:32:30 +0200

    ` init`

--- src/Application/Public/Module/Microphone.js
+++ src/Application/Public/Module/Microphone.js
@@ -152,6 +152,7 @@ microphone.init = (section_id) => {
                                 a.data('method', '');
                                 a.data('target', '');
                                 a.data('header-with-authorization', 'true');
+                                clip_container.appendChild(a);
                                 a.trigger('click');
                                 //<a data-url="https://admin.workandtravel.local/Application/Filemanager/" data-method="" data-target="" data-header-with-authorization="true" data-mtime="1778145542.239"><li class="item"><p><img src="/Application/Filemanager/Icon/Icon.png" alt="icon" class="icon">Filemanager</p></li></a>
                                 // request(response.url);
